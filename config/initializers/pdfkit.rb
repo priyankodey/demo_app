4e98d62ef43a2de8b183b32b90d9a5e2004d439b
priyanko <priynko.priyankodey87@gmail.com>
2014-02-10 17:43:31 +0530

    creating on heroku

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,17 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
 # config.wkhtmltopdf = "#{}"Rails.root.join(‘vendor’, ‘wkhtmltopdf-amd64’).to_s if Rails.env.production?
-config.wkhtmltopdf = Rails.root.join('vendor', 'bin', wkhtmltopdf_executable).to_s
+
+if RUBY_PLATFORM =~ /linux/
+    wkhtmltopdf_executable = 'wkhtmltopdf-amd64'
+  elsif RUBY_PLATFORM =~ /darwin/
+    wkhtmltopdf_executable = 'wkhtmltopdf-osx'
+  else
+    raise "Unsupported. Must be running linux or intel-based Mac OS."
+  end
+  config.wkhtmltopdf = Rails.root.join('vendor', 'bin', wkhtmltopdf_executable).to_s
+end
+
 # Rails.root.join(‘vendor’, ‘wkhtmltopdf-amd64’).to_s if Rails.env.production?
   config.wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
   config.default_options = {
